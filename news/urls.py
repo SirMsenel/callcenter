1d8ed43fe9321ba109d4fe661aa1e671c9517f65
mehmet şenel <senelm263@gmail.com>
2024-11-17 16:03:17 +0300

    navbarpositon

--- news/urls.py
+++ news/urls.py
@@ -11,3 +11,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),  # Giriş işlemi
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),  # Çıkış işlemi 
 ]
+
+
+# urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+# Eğer proje geliştirme aşamasında ise
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
